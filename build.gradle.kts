e52787f8eb15c01d7286799673df7b74a508026b
shamila37 <128484072+shamila37@users.noreply.github.com>
2024-06-20 12:31:17 +0530

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-//    testImplementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
-//    implementation("com.github.cliftonlabs:json-simple:4.0.1")
 }
 
 tasks.test {
